fdb596a8e73c4256319b6d6afe338dda704ab0b7
NERO <NERO@NERO2>
2021-04-23 21:59:11 +0200

    Inizial commit of progect.

---
+++ ProvaGit/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//creazione del java project
+		System.out.println("QUESTA E' UNA PROVA PER TESTARE GIT E GITHUB");
+		
+		/*creazione della repo in locale
+		 * team > share project	
+		 */
+	}
+
+}
