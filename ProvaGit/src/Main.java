6e0d1cb75691bf9c58b20a0d014f6a2cd3abb91a
NERO <NERO@NERO2>
2021-04-23 22:26:43 +0200

    Added new suggestions.

--- ProvaGit/src/Main.java
+++ ProvaGit/src/Main.java
@@ -7,6 +7,24 @@ public class Main {
 		
 		/*creazione della repo in locale
 		 * team > share project	
+		 * 
+		 * quando creo la repo locale eclipse mi sposta il progetto al suo interno
+		 * 
+		 * 
+		 * Git Staging: seleziono i files che voglio aggiungere all'index ed in seguito committare
+		 * 
+		 * Attenzione!! prima di fare il push all'interno della repo assicurati che la repo sia esistente
+		 *              in caso contrario creala (durante la creazione, se esiste già la repository locale,
+		 *              non selezionare l'aggiunta del file READ perchè causerà un commit iniziale che permette
+		 *              la clonazione della repo (cosa non necessaria perchè il primo commit lo facciamo noi))
+		 *              
+		 * creiamo un riferimento remoto fornendo il link https che fornisce il percorso per raggiungere la repo
+		 * creata su gitHub. Dobbiamo specificare anche il riferimento del push che può essere il master o un
+		 * branch a nostra scelta (a patto che esista)
+		 * 
+		 * eseguire il push 
+		 * 
+		 * 
 		 */
 	}
 
